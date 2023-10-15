4c6a11848ebb9c73fe4b66f636404d2a97483adf
ThivyaRaj18 <147935358+ThivyaRaj18@users.noreply.github.com>
2023-10-15 09:20:31 +0800

    Add files via upload

---
+++ assignments/Browser.java
@@ -0,0 +1,22 @@
+package week1.day1.assignments;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		//declaring variables
+		float browserVersion = 108.5f;
+		String browserName = "Firefox";
+		boolean pageLoaded = true;
+		int noOfBrowser = 1;
+		char browserLogo = 'F';
+		//Printing the variables
+		System.out.println(browserVersion);
+		System.out.println(browserName);
+		System.out.println(pageLoaded);
+		System.out.println(noOfBrowser);
+		System.out.println(browserLogo);
+
+
+	}
+
+}
---
+++ assignments/FindNumber.java
@@ -0,0 +1,21 @@
+package week1.day1.assignments;
+
+public class FindNumber {
+
+	public static void main(String[] args) {
+		//Variable declaration
+
+		int num = 45;
+
+		//Using conditional statement to check the number
+
+		if (num>0) {
+			System.out.println("The number is positive");
+		}
+		else {
+			System.out.println("The number is negative ");
+		}
+
+	}
+
+}
